dcf667d09b01778a9e6b03e473bbd8c70b1b91e0
wangjie5540 <278832221@qq.com>
2015-08-21 19:20:13 +0800

    去掉jni目录

--- vod/BfCloudPlayer/gen/bf/cloud/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package bf.cloud.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- vod/BfCloudPlayer/jni/Application.mk
+++
@@ -1,4 +0,0 @@
-APP_STL := stlport_static 
-APP_ABI := armeabi
-APP_PLATFORM := android-9
-APP_SHORT_COMMANDS := true
