4520dc93656b5da11106cc8e9c8106427f784300
wangjie5540 <278832221@qq.com>
2015-11-03 11:48:54 +0800

    1、去掉StreamDemo 2、优化demo

--- black_board/src/bf/cloud/black_board/MainActivity.java
+++ black_board/src/bf/cloud/black_board/MainActivity.java
@@ -12,15 +12,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		findViewById(R.id.stream_demo).setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent();
-				intent.setClass(MainActivity.this, StreamDemo.class);
-				startActivity(intent);
-			}
-		});
 		findViewById(R.id.black_board_demo).setOnClickListener(new OnClickListener() {
 			
 			@Override
--- black_board/src/bf/cloud/black_board/VodDemo.java
+++ black_board/src/bf/cloud/black_board/VodDemo.java
@@ -11,26 +11,17 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.Toast;
 
-public class VodDemo extends Activity implements PlayErrorListener, PlayEventListener{
+/**
+ * 
+ * @author BfCloud. Note: Please set your project to Utf8
+ * 
+ */
+public class VodDemo extends Activity implements PlayErrorListener,
+		PlayEventListener {
 	private final String TAG = VodDemo.class.getSimpleName();
 	private VodPlayer mPlayer = null;
-	private Button btChangeDecodeMode = null;
-	private Button btStart = null;
-	private Button btStop = null;
-	private Button btPause = null;
-	private Button btResume = null;
-	private Button btSeekTo = null;
-	private Button btIncVolume = null;
-	private Button btDecVolume = null;
-	private Button btGetCurVolume = null;
-	private Button btGetMAZVolume = null;
-	private Button btGetCurrentPosition = null;
-	private Button btGetDuration = null;
-	private Button btChangePlayerType = null;
 	private boolean mVrFlag = false;
 	private Context mContext = null;
 	// private String mUrls =
@@ -44,178 +35,140 @@ public class VodDemo extends Activity implements PlayErrorListener, PlayEventLis
 		init();
 	}
 
+	public void onClick(View view) {
+		int id = view.getId();
+		switch (id) {
+		case R.id.change_decode_mode: {
+			String[] items = { "自动(ExoPlayer优先)", "软解(ffmpeg)" };
+			int checkedItem = -1;
+			Log.d(TAG, "mPlayer.getDecodeMode():" + mPlayer.getDecodeMode());
+			if (mPlayer.getDecodeMode() == DecodeMode.AUTO) {
+				checkedItem = 0;
+			} else {
+				checkedItem = 1;
+			}
+			new AlertDialog.Builder(mContext)
+					.setSingleChoiceItems(items, checkedItem, null)
+					.setPositiveButton("确认",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									dialog.dismiss();
+									mPlayer.stop();
+									int position = ((AlertDialog) dialog)
+											.getListView()
+											.getCheckedItemPosition();
+									if (position == 0) {
+										mPlayer.setDecodeMode(DecodeMode.AUTO);
+									} else if (position == 1) {
+										mPlayer.setDecodeMode(DecodeMode.SOFT);
+									}
+								}
+							})
+					.setNegativeButton("取消",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									dialog.dismiss();
+								}
+							}).show();
+			break;
+		}
+		case R.id.start:{
+			Log.d(TAG, "Start onClick");
+			mPlayer.stop();
+			mPlayer.setVrFlag(mVrFlag);
+			mPlayer.setDataSource(mUrls);
+			mPlayer.start();
+		}
+		case R.id.stop:
+			mPlayer.stop();
+			break;
+		case R.id.pause:
+			mPlayer.pause();
+			break;
+		case R.id.resume:
+			mPlayer.resume();
+			break;
+		case R.id.seekto:
+			mPlayer.seekTo(30000);
+			break;
+		case R.id.inc_volume:
+			mPlayer.incVolume();
+			break;
+		case R.id.dec_volume:
+			mPlayer.decVolume();
+			break;
+		case R.id.get_current_volume:{
+			int value = mPlayer.getCurrentVolume();
+			Toast.makeText(VodDemo.this, "" + value, Toast.LENGTH_SHORT)
+					.show();
+			break;
+		}
+		case R.id.get_max_volume:{
+			int value = mPlayer.getMaxVolume();
+			Toast.makeText(VodDemo.this, "" + value, Toast.LENGTH_SHORT)
+					.show();
+			break;
+		}
+		case R.id.get_cur_position:{
+			long value = mPlayer.getCurrentPosition();
+			Toast.makeText(VodDemo.this, "" + value, Toast.LENGTH_SHORT)
+					.show();
+			break;
+		}
+		case R.id.get_duration:{
+			long value = mPlayer.getDuration();
+			Toast.makeText(VodDemo.this, "" + value, Toast.LENGTH_SHORT)
+					.show();
+			break;
+		}
+		case R.id.change_player_type:{
+			String[] items = new String[] { "切换至普通播放器", "切换至全景播放器" };
+			new AlertDialog.Builder(mContext)
+					.setSingleChoiceItems(items, mVrFlag ? 1 : 0, null)
+					.setPositiveButton("确认",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									dialog.dismiss();
+									int position = ((AlertDialog) dialog)
+											.getListView()
+											.getCheckedItemPosition();
+									if (position == 0) {
+										mVrFlag = false;
+									} else if (position == 1) {
+										mVrFlag = true;
+									}
+									mPlayer.stop();
+									mPlayer.setVrFlag(mVrFlag);
+								}
+							})
+					.setNegativeButton("取消",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									dialog.dismiss();
+								}
+							}).show();
+			break;
+		}
+		default:
+			break;
+		}
+	}
+
 	private void init() {
 		mContext = this;
-		btChangeDecodeMode = (Button)findViewById(R.id.change_decode_mode);
-		btChangeDecodeMode.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				String[] items = {"自动(ExoPlayer优先)", "软解(ffmpeg)"};
-				int checkedItem = -1;
-				Log.d(TAG, "mPlayer.getDecodeMode():" + mPlayer.getDecodeMode());
-				if (mPlayer.getDecodeMode() == DecodeMode.AUTO){
-					checkedItem = 0;
-				}else{
-					checkedItem = 1;
-				}
-				new AlertDialog.Builder(mContext)
-					.setSingleChoiceItems(items, checkedItem, null)
-					.setPositiveButton("确认", new DialogInterface.OnClickListener() {
-						
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							dialog.dismiss();
-							mPlayer.stop();
-							int position = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
-							if (position == 0){
-								mPlayer.setDecodeMode(DecodeMode.AUTO);
-							} else if (position == 1){
-								mPlayer.setDecodeMode(DecodeMode.SOFT);
-							}
-						}
-					})
-					.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-						
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							dialog.dismiss();
-						}
-					}).show();
-			}
-		});
-		btStart = (Button) findViewById(R.id.start);
-		btStart.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Log.d(TAG, "Start onClick");
-				mPlayer.stop();
-				mPlayer.setVrFlag(mVrFlag);
-				mPlayer.setDataSource(mUrls);
-				mPlayer.start();
-			}
-		});
-		btStop = (Button) findViewById(R.id.stop);
-		btStop.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				mPlayer.stop();
-			}
-		});
-		btPause = (Button) findViewById(R.id.pause);
-		btPause.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				mPlayer.pause();
-			}
-		});
-		btResume = (Button) findViewById(R.id.resume);
-		btResume.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				mPlayer.resume();
-			}
-		});
-		btSeekTo = (Button) findViewById(R.id.seekto);
-		btSeekTo.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				mPlayer.seekTo(30000);
-			}
-		});
-		btIncVolume = (Button) findViewById(R.id.inc_volume);
-		btIncVolume.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				mPlayer.incVolume();
-			}
-		});
-		btDecVolume = (Button) findViewById(R.id.dec_volume);
-		btDecVolume.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				mPlayer.decVolume();
-			}
-		});
-		btGetCurVolume = (Button) findViewById(R.id.get_current_volume);
-		btGetCurVolume.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				int value = mPlayer.getCurrentVolume();
-				Toast.makeText(VodDemo.this, "" + value, Toast.LENGTH_SHORT)
-						.show();
-			}
-		});
-		btGetMAZVolume = (Button) findViewById(R.id.get_max_volume);
-		btGetMAZVolume.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				int value = mPlayer.getMaxVolume();
-				Toast.makeText(VodDemo.this, "" + value, Toast.LENGTH_SHORT)
-						.show();
-			}
-		});
-		btGetCurrentPosition = (Button) findViewById(R.id.get_cur_position);
-		btGetCurrentPosition.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				long value = mPlayer.getCurrentPosition();
-				Toast.makeText(VodDemo.this, "" + value, Toast.LENGTH_SHORT)
-						.show();
-			}
-		});
-		btGetDuration = (Button) findViewById(R.id.get_duration);
-		btGetDuration.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				long value = mPlayer.getDuration();
-				Toast.makeText(VodDemo.this, "" + value, Toast.LENGTH_SHORT)
-						.show();
-			}
-		});
-		btChangePlayerType = (Button) findViewById(R.id.change_player_type);
-		btChangePlayerType.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				String[] items = new String[] { "切换至普通播放器", "切换至全景播放器" };
-				new AlertDialog.Builder(mContext)
-					.setSingleChoiceItems(items, mVrFlag ? 1 : 0, null)
-					.setPositiveButton("确认", new DialogInterface.OnClickListener() {
-						
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							dialog.dismiss();
-							int position = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
-							if (position == 0){
-								mVrFlag = false;
-							}else if (position == 1){
-								mVrFlag = true;
-							}
-							mPlayer.stop();
-							mPlayer.setVrFlag(mVrFlag);
-						}
-					})
-					.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-						
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							dialog.dismiss();
-						}
-					}).show();
-			}
-		});
+		// 获取播放器
 		mPlayer = (VodPlayer) findViewById(R.id.vod_player);
 		mPlayer.registPlayErrorListener(this);
 		mPlayer.registPlayEventListener(this);
